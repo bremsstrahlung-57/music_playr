2d3da6fa03661c18049e725e68e3e1c058ddffea
bremsstrahlung-57 <sagar292905@gmail.com>
2025-10-29 23:50:21 +0530

    feat: add music file input and confirmation modal to player interface

--- src/player.cpp
+++ src/player.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <taglib/fileref.h>
 #include <vector>
 
 #include "audio.hpp"
@@ -13,6 +14,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "imgui_internal.h"
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
 
 int main_window() {
@@ -122,6 +124,8 @@ int main_window() {
       ImGui::Text("No tracks available");
     }
 
+    ImGui::Separator();
+
     if (ImGui::Button("Play/Pause") && found) {
       // play_from_selectable:
       main_player.play(current_song.file_path, current_song.id);
@@ -139,6 +143,7 @@ int main_window() {
       }
       play_next_track();
     }
+
     ImGui::SameLine();
     if (ImGui::Button("Previous")) {
       main_player.stop();
@@ -147,6 +152,8 @@ int main_window() {
       state.last_track_id = current_song.id;
       main_player.play(current_song.file_path, current_song.id);
     }
+
+    ImGui::Separator();
 
     float vol = main_player.get_volume();
     if (ImGui::SliderFloat("Volume", &vol, 0.0f, 1.0f)) {
@@ -166,7 +173,9 @@ int main_window() {
       seek_value = live;
     }
 
-    if (ImGui::SliderFloat("Seek", &seek_value, 0.0f, total)) {
+    ImGui::Separator();
+
+    if (ImGui::SliderFloat("##float", &seek_value, 0.0f, total)) {
       is_seeking = true;
     }
 
@@ -194,6 +203,8 @@ int main_window() {
       main_database.save_app_state(state);
     }
 
+    ImGui::Separator();
+
     if (ImGui::Button("Exit")) {
       state.last_track_id = current_song.id;
       state.volume = main_player.get_volume();
@@ -204,6 +215,104 @@ int main_window() {
     ImGui::End();
 
     ImGui::Begin("Queue");
+
+    static char buf[128];
+    static ImGuiInputTextFlags flags = ImGuiInputTextFlags_ElideLeft;
+    static AudioMetadata _metadata;
+
+    ImGui::InputText("Path to file", buf, IM_ARRAYSIZE(buf), flags);
+
+    ImGui::PushStyleColor(ImGuiCol_Button,
+                          (ImVec4)ImColor::HSV(0.7f, 0.6f, 0.6f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered,
+                          (ImVec4)ImColor::HSV(0.7f, 0.7f, 0.7f));
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive,
+                          (ImVec4)ImColor::HSV(0.7f, 0.8f, 0.8f));
+
+    std::string buf_path(buf);
+
+    if (ImGui::Button("Add Music")) {
+      if (buf_path.empty()) {
+        ImGui::OpenPopup("Empty");
+      } else if (TagLib::FileRef(buf).isNull()) {
+        ImGui::OpenPopup("Wrong");
+      } else {
+        _metadata = main_database.get_metadata(buf);
+        ImGui::OpenPopup("Add");
+      }
+    }
+
+    ImGui::PopStyleColor(3);
+
+    ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+    ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+
+    if (ImGui::BeginPopupModal("Add", NULL,
+                               ImGuiWindowFlags_AlwaysAutoResize)) {
+      ImGui::Text("Confirm Addition?");
+
+      ImGui::Separator();
+
+      ImGui::Text("Path: %s", buf);
+      ImGui::Bullet();
+      ImGui::Text("Title: %s", _metadata.title.c_str());
+      ImGui::Bullet();
+      ImGui::Text("Artist: %s", _metadata.artist.c_str());
+      ImGui::Bullet();
+      ImGui::Text("Length: %ds", _metadata.length_in_seconds);
+
+      ImGui::Separator();
+
+      static bool dont_ask_me_next_time = false;
+      ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
+      ImGui::Checkbox("Don't ask me next time", &dont_ask_me_next_time);
+      ImGui::PopStyleVar();
+
+      if (ImGui::Button("OK", ImVec2(120, 0))) {
+        main_database.add_track(buf);
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::SetItemDefaultFocus();
+      ImGui::SameLine();
+      if (ImGui::Button("Cancel", ImVec2(120, 0))) {
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::EndPopup();
+    } else if (ImGui::BeginPopupModal("Empty", NULL,
+                                      ImGuiWindowFlags_AlwaysAutoResize)) {
+      ImGui::Text("File Path Empty");
+      ImGui::Separator();
+      if (ImGui::Button("OK", ImVec2(120, 0))) {
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::SetItemDefaultFocus();
+      ImGui::SameLine();
+      if (ImGui::Button("Cancel", ImVec2(120, 0))) {
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::EndPopup();
+    } else if (ImGui::BeginPopupModal("Wrong", NULL,
+                                      ImGuiWindowFlags_AlwaysAutoResize)) {
+      ImGui::TextWrapped("Invalid audio file:");
+      ImGui::Bullet();
+      ImGui::Text("File may not exist");
+      ImGui::Bullet();
+      ImGui::Text("Unsupported format");
+      ImGui::Bullet();
+      ImGui::Text("File may be corrupted");
+      ImGui::Separator();
+      if (ImGui::Button("OK", ImVec2(120, 0))) {
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::SetItemDefaultFocus();
+      ImGui::SameLine();
+      if (ImGui::Button("Cancel", ImVec2(120, 0))) {
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::EndPopup();
+    }
+
+    ImGui::Separator();
 
     for (const auto &track : ALL_TRACKS) {
       if (ImGui::Selectable(track.title.c_str())) {
